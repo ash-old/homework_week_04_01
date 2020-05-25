1727f75acae019e6c10542a28fed9f6871c50713
ash-old <ashley.oldham@gmail.com>
2020-05-25 16:52:52 +0100

    welcome page added

--- controller.rb
+++ controller.rb
@@ -3,12 +3,25 @@ require('sinatra/contrib/all')
 require('pry-byebug')
 
 require_relative('./models/game')
+also_reload('./models/*')
+
+
+get '/welcome' do
+  erb(:welcome)
+end
+
 
 # get '/rock/scissors' do
 #   "rock wins"
 # end
 
+#no erb file
+# get '/game/:move1/:move2' do
+#   return "#{Game.rock_paper_scissors(params[:move1], params[:move2])}"
+# end
 
+#with erb file
 get '/game/:move1/:move2' do
-  return "#{Game.rock_paper_scissors(params[:move1], params[:move2])}"
+  @game_result = Game.rock_paper_scissors(params[:move1], params[:move2])
+  erb(:result)
 end
